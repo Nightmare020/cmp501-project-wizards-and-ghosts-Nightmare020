16edc66df8122549291412eb28e32e92977fdf94
Nightmare020 <albalexbelen@gmail.com>
2025-02-10 19:48:18 +0000

    Clone client project added

---
+++ Help From Beyond Client_clone_0/Assets/Scripts/Networking/MatchmakingClient.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.AspNetCore.SignalR.Client;
+using Unity.Netcode;
+
+public class MatchmakingClient : MonoBehaviour
+{
+    private HubConnection _hubConnection;
+    private SelectionWizardGhost _selectionPanel;
+    public static MatchmakingClient Instance { get; private set; }
+
+    public string MatchedRole { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        _selectionPanel = FindObjectOfType<SelectionWizardGhost>();
+    }
+    // Start is called before the first frame update
+    private async void Start()
+    {
+        Debug.Log("Initializing hub connection...");
+
+        _hubConnection = new HubConnectionBuilder()
+            .WithUrl("http://localhost:5077/matchmaking") // replace with my server URL
+            .WithAutomaticReconnect()
+            .Build();
+
+        Debug.Log("Hub connection initialized");
+
+        _hubConnection.On<string>("Matched", OnMatched);
+        Debug.Log("Listening for 'Matched' events");
+
+        try
+        {
+            await _hubConnection.StartAsync();
+            Debug.Log("Connected to server");
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError($"Failed to connect to server: {ex.Message}");
+        }
+    }
+
+    public async void SelectRole(string role)
+    {
+        if (_hubConnection == null)
+        {
+            Debug.LogError("Hub connection is not initialized yet");
+        }
+
+        if (_hubConnection.State == HubConnectionState.Connected)
+        {
+            await _hubConnection.InvokeAsync("SelectRole", role);
+        }
+        else
+        {
+            Debug.LogError("Not connected to server");
+        }
+    }
+
+    private void OnMatched(string role)
+    {
+        MatchedRole = role;
+        Debug.Log($"Matched as {role}");
+
+        // Notify character selection class that a match was found
+        UnityMainThreadDispatcher.Instance.Enqueue(() =>
+        {
+            SelectionWizardGhost.NotifyPlayerFound(true);
+        });
+    }
+}
