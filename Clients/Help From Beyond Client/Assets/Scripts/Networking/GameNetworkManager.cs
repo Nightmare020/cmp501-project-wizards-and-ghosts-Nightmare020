b5c524cbb922e6db1ba2045e085ebaaef2779efa
Nightmare020 <albalexbelen@gmail.com>
2025-02-15 21:13:23 +0000

    More set up for client-server logic

---
+++ Clients/Help From Beyond Client/Assets/Scripts/Networking/GameNetworkManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class GameNetworkManager : MonoBehaviour
+{
+    public bool runAsServer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Application.isBatchMode || runAsServer)
+        {
+            Debug.Log("Starting as a dedicated server...");
+            NetworkManager.Singleton.StartServer();
+        }
+        else
+        {
+            Debug.Log("Starting as client...");
+            NetworkManager.Singleton.StartClient();
+        }
+    }
+}
