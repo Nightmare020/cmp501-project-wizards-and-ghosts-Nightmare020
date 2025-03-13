52016923098883ec266cdfcb91956f675e1f7f9d
Nightmare020 <albalexbelen@gmail.com>
2025-03-13 22:21:41 +0100

    Revert "Revert all the shit"
    This reverts commit f5f380caae3985240c0576559367ceaa76303edc.

---
+++ Clients/Help From Beyond Client_clone_0/Assets/Scripts/Networking/GameNetworkManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine;
+
+public class GameNetworkManager : MonoBehaviour
+{
+    public bool runAsServer = false;
+
+    public string serverIP = "127.0.0.1"; // Local server for testing
+    public ushort serverPort = 7777; // Default Netcode port
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+
+        Debug.Log("Starting as client...");
+        transport.SetConnectionData(serverIP, serverPort); // Connect to server
+        NetworkManager.Singleton.StartClient();
+        Debug.Log($"Client trying to connect to {serverIP}:{serverPort}");
+    }
+}
