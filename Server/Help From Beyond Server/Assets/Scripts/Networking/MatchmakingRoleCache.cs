0ff28f319a3473d28ea2bc3530fa7059f29b2291
Nightmare020 <albalexbelen@gmail.com>
2025-04-08 02:19:31 +0200

    Deleted unnecesary class

--- Server/Help From Beyond Server/Assets/Scripts/Networking/MatchmakingRoleCache.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class MatchmakingRoleCache
-{
-    public static Dictionary<ulong, PlayerState> clientRoles = new();
-}
